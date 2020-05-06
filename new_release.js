43b9b32d6abb7fb75f33b92b5204d85b6e77ad3c
niihen-dzg <64913696+niihen-dzg@users.noreply.github.com>
2020-05-06 22:14:09 +0700

    Create new_release.js

---
+++ new_release.js
@@ -0,0 +1,19 @@
+window.onload = function() { 
+
+$('#rilis').each(function(){ 
+var hasilrilis = "<div class='rlsn'><table class='rilisan'><tr>"+
+
+
+"<td><a href='"+l1+"'><img src='"+g1+"' alt='"+j1+"'/><div class='judul-rilisan'>"+j1+"</div></a></td>"+
+"<td><a href='"+l2+"'><img src='"+g2+"' alt='"+j2+"'/><div class='judul-rilisan'>"+j2+"</div></a></td>"+
+"<td><a href='"+l3+"'><img src='"+g3+"' alt='"+j3+"'/><div class='judul-rilisan'>"+j3+"</div></a></td>"+
+"<td><a href='"+l4+"'><img src='"+g4+"' alt='"+j4+"'/><div class='judul-rilisan'>"+j4+"</div></a></td>"+
+"<td><a href='"+l5+"'><img src='"+g5+"' alt='"+j5+"'/><div class='judul-rilisan'>"+j5+"</div></a></td>"+
+
+
+"</tr></table></div>";
+$(this).attr("id", "card-release"); 
+document.getElementById("card-release").innerHTML = hasilrilis;
+});
+
+};
